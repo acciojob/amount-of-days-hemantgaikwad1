2dd9b094904a9a9e902407fab78842d3b600ceb6
Hemant Gaikwad <103812753+hemantgaikwad1@users.noreply.github.com>
2026-08-03 18:06:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-function daysOfYear( year){
+function daysOfAYear( year){
 	if(year%100!=0 && year%4===0){
 		return 366;
 	}
@@ -8,3 +8,4 @@ function daysOfYear( year){
 	}
 	return 365;
 }
+daysOfAYear(2000);
